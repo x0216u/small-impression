9d09214a7b36acfb57c9a67a05a2801ddcef3632
x0216u <940120528@qq.com>
2018-09-04 00:21:55 +0800

    6

--- pages/award/award.js
+++ pages/award/award.js
@@ -5,9 +5,12 @@ let canRoll = true, //加控制，防止用户点击两次
   lottery = ['奖品1', '奖品2', '奖品3']; //放奖品
 var context = wx.createCanvasContext('firstCanvas')
 var colors = ['#9e9bd2', '#f7a172', '#F1A68E', '#a3d8d6', '#a035b6', '#3A97EA', '#FAA167', '#63CA97', '#9A50B3'];
+var rale = wx.getSystemInfoSync().windowWidth / 750
+var w = 750 * rale
+var h = 750 * rale
 Page({
   data: {
-    circleList: [], //圆点数组
+    canRoll:true,
     awardList: [{name: '拍立得套装',color: colors[0]},
     {
       name: '旅行便携套装',
@@ -30,16 +33,22 @@ Page({
     colorCircleSecond: '#FE4D32' //圆点颜色2
   },
   onReady: function(e) {
-   
+    var that =this;
+    wx.createSelectorQuery().select('#canvas-container').boundingClientRect(function (rect) {//监听canvas的宽高 
+      w = rect.width //获取canvas宽度的一半；
+      h = rect.height //获取canvas高度的一半
+      that.loadCanvas();
+      context.translate(w / 2, h/2);
+    }).exec()
+
+  },
+  loadCanvas:function(){
+
     var that = this;
     //定义人名和对应的颜色
     var data = this.data.awardList;
-    // 使用 wx.createContext 获取绘图上下文 context
-    var rale = wx.getSystemInfoSync().windowWidth / 750
     var len = data.length;
     var angle = 360 / len;
-    var w = 750 * rale
-    var h = 750 * rale
     var radius = 300 * rale
     var ctx = context;
     for (var i = 0; i < len; i++) {
@@ -48,11 +57,9 @@ Page({
       ctx.moveTo(w / 2, h / 2);
       ctx.arc(w / 2, h / 2, radius, angle * i * Math.PI / 180, (angle + angle * i) * Math.PI / 180); //绘制扇形，注意下一个扇形比上一个扇形多一个itemsArc的角度。
       ctx.closePath();
-      if (i % 2 == 0) {
-        ctx.setFillStyle("#FBF1A9");
-      } else {
-        ctx.setFillStyle("#fbb936");
-      }
+
+      ctx.setFillStyle(item.color);
+
       ctx.fill();
       ctx.save();
       ctx.beginPath();
@@ -62,9 +69,9 @@ Page({
       ctx.setTextBaseline("middle"); //使文字水平居中显示
       ctx.translate(w / 2, h / 2); //将原点移至圆形圆心位置
       ctx.rotate((angle * (i + 2) - 20) * Math.PI / 180); //旋转文字，从 i+2 开始，因为扇形是从数学意义上的第四象限第一个开始的，文字目前的位置是在圆心正上方，所以起始位置要将其旋转2个扇形的角度让其与第一个扇形的位置一致。
-      ctx.fillText(item.name, 0, -(h / 2 * 0.8));
+      ctx.fillText(item.name, 0, -(h / 2 * 0.7));
       ctx.restore(); //保存绘图上下文，使上一个绘制的扇形保存住。
-     
+
 
     }
     that.loadImage();
@@ -75,17 +82,21 @@ Page({
     let that = this;
     let data = this.data.awardList;
     let Num = data.length;
-    let angel  = 360 / len;
+    let angel = 360 / Num;
     let ctx = context;
     for (let i = 0; i < Num; i++) {
       ctx.save();
       ctx.beginPath();
-      ctx.translate(w1, h1);
-      ctx.rotate(itemsArc * (i + 2) * Math.PI / 180);
-      ctx.drawImage("/icons/img/award.jpg", -(w1 * 0.2), -(h1 * 0.6), (w1 * 0.4), (h1 * 0.2));
+      ctx.translate(w/2, h/2);
+      ctx.rotate((angel * (i + 2) - 20) * Math.PI / 180 );
+      ctx.drawImage("/icons/img/award.jpg", -(w / 2 * 0.15), -(h / 2 * 0.5), (w / 2 * 0.3), (h / 2 * 0.15));
       ctx.restore();
     }
 
+
+  },
+  touchCanvas:function(e){
+    this.startAward()
   },
   onLoad: function(opt) {
     var _this = this;
@@ -130,57 +141,39 @@ Page({
     }, 500)
     // this.setPlateData(); //执行设置转盘表面的文字
     // let that = this;
-    // let aniData = wx.createAnimation({ //创建动画对象
-    //   duration: 2000,
-    //   timingFunction: 'ease'
-    // });
-    // this.aniData = aniData; //将动画对象赋值给this的aniData属性
+    let aniData = wx.createAnimation({ //创建动画对象
+      duration: 2000,
+      timingFunction: 'ease'
+    });
+    this.aniData = aniData; //将动画对象赋值给this的aniData属性
   },
-  setPlateData() { //设置奖品数组
-    lotteryArrLen = lottery.length; //获取奖品数组的长度，用来判断
-    if (lotteryArrLen < 2) { //数组的奖品只有一个，扩展数组的长度到4
-      let evenArr = new Array(4); //创建一个数组，方便操作。
-      for (let i = 0; i < 4; i) {
-        if (i % 2 == 1) { //这里为什么要取1是为了在默认的界面将指针放在谢谢的地方，防止别人拿着中奖的截图来要奖品
-          evenArr[i] = lottery[0]; //将原数组的内容赋值到新的数组
-        } else {
-          evenArr[i] = '谢谢' //在数组中间隔插入谢谢
-        }
-      }
-      lottery = [...evenArr]; //将整合好的数组赋值给lottery数组
-    } else { //数组中的奖品超过1个，则正常扩展数组，扩展的数组为原来的2倍
-      let dataLen = 0; //用来放原来数组的索引
-      let evenArr = new Array(lotteryArrLen * 2); //创建扩展数组
-      for (let i = 0; i < (lotteryArrLen * 2); i) {
-        if (i % 2 == 1) {
-          evenArr[i] = lottery[dataLen]; //将原来数组的值赋值给新数组
-          dataLen; //原来数组的索引加一
-        } else {
-          evenArr[i] = '谢谢'
-        }
-      }
-      lottery = [...evenArr]; //将整合好的数组赋值给lottery数组
+  startAward:function(){
+    var canRoll = this.data.canRoll
+    if (canRoll){
+      this.setData({
+        canRoll: false
+      })
+      let aniData = this.aniData; //获取this对象上的动画对象
+      let rightNum = ~~(Math.random() * 5); //生成随机数
+      console.log(`随机数是${rightNum}`);
+      // console.log(`奖品是：${lottery[rightNum]}`);
+      aniData.rotate(3600 * num - 360 / 5 * rightNum).step(); //设置转动的圈数
+      this.setData({
+        aniData: aniData.export() 
+      })
+      this.setData({
+        canRoll: true
+      })
     }
-
-    lotteryArrLen = lottery.length; //获取新的数组长度
-    this.setData({
-      lottery: lottery //设置好值，用于页面展示
-    })
   },
   startRollTap() { //开始转盘
     let that = this;
+    var canRoll = true;
     if (canRoll) {
       canRoll = false;
-      let aniData = this.aniData; //获取this对象上的动画对象
-      let rightNum = ~~(Math.random() * lotteryArrLen); //生成随机数
-      console.log(`随机数是${rightNum}`);
-      console.log(`奖品是：${lottery[rightNum]}`);
-      aniData.rotate(3600 * num - 360 / lotteryArrLen * rightNum).step(); //设置转动的圈数
-      this.setData({
-        aniData: aniData.export()
-      })
+      
       num;
-      canRoll = true;
+      
     }
   }
 })
