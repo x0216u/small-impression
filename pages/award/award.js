6fc10a5340ce6cb84f946805a231d6614bbb159d
x0216u <940120528@qq.com>
2018-09-03 06:32:50 -0400

    5

--- pages/award/award.js
+++ pages/award/award.js
@@ -3,83 +3,119 @@ let canRoll = true, //加控制，防止用户点击两次
   num = 1, //用在动画上，让用户在第二次点击的时候可以接着上次转动的角度继续转
   lotteryArrLen = 0, //放奖品的数组的长度
   lottery = ['奖品1', '奖品2', '奖品3']; //放奖品
+var context = wx.createCanvasContext('firstCanvas')
+var colors = ['#9e9bd2', '#f7a172', '#F1A68E', '#a3d8d6', '#a035b6', '#3A97EA', '#FAA167', '#63CA97', '#9A50B3'];
 Page({
   data: {
-    circleList: [],//圆点数组
-    awardList: [],//奖品数组
-    colorCircleFirst: '#FFDF2F',//圆点颜色1
-    colorCircleSecond: '#FE4D32'//圆点颜色2
+    circleList: [], //圆点数组
+    awardList: [{name: '拍立得套装',color: colors[0]},
+    {
+      name: '旅行便携套装',
+      color: colors[1]
+    },
+    {
+      name: '1000金币',
+      color: colors[2]
+    },
+    {
+      name: '400金币',
+      color: colors[3]
+    },
+    {
+      name: '50金币',
+      color: colors[4]
+    }
+    ], //奖品数组
+    colorCircleFirst: '#FFDF2F', //圆点颜色1
+    colorCircleSecond: '#FE4D32' //圆点颜色2
   },
-  onReady: function (e) {
-    var colors = ['#33ACB5', '#DF5C5D', '#B4D966', '#5450B3', '#FFD457', '#3A97EA', '#FAA167', '#63CA97', '#9A50B3'];
+  onReady: function(e) {
+   
+    var that = this;
     //定义人名和对应的颜色
-    var data = [
-      { name: '科比', color: colors[0] },
-      { name: '科比', color: colors[1] },
-      { name: '科比', color: colors[2] },
-      { name: '科比', color: colors[4] },
-      { name: '科比', color: colors[5] }
-    ];
+    var data = this.data.awardList;
     // 使用 wx.createContext 获取绘图上下文 context
-    var context = wx.createCanvasContext('firstCanvas')
     var rale = wx.getSystemInfoSync().windowWidth / 750
     var len = data.length;
-    var angle = Math.PI * 2 / len;
+    var angle = 360 / len;
     var w = 750 * rale
     var h = 750 * rale
     var radius = 300 * rale
-    for (var i = 0; i <len; i++) {
+    var ctx = context;
+    for (var i = 0; i < len; i++) {
       var item = data[i];
-      context.setStrokeStyle(item.color)
-      context.setLineWidth(1)
-      context.moveTo(w/2, h/2);
-      var topCircle = h/2 - radius * Math.sin(angle * i);
-      var leftCircle = w/2 + radius * Math.cos(angle * i);
-      //context.lineTo(leftCircle, topCircle);
-      console.info(leftCircle + ':'+topCircle)
-
-      var start = angle * (len - i - 1)
-      var end = angle*(len-i)
-      console.info(start)
-      console.info(end)
-      context.arc(w / 2, h / 2, radius, start, end, true)
-      context.lineTo(w / 2, h / 2);
-      context.stroke()
+      ctx.beginPath();
+      ctx.moveTo(w / 2, h / 2);
+      ctx.arc(w / 2, h / 2, radius, angle * i * Math.PI / 180, (angle + angle * i) * Math.PI / 180); //绘制扇形，注意下一个扇形比上一个扇形多一个itemsArc的角度。
+      ctx.closePath();
+      if (i % 2 == 0) {
+        ctx.setFillStyle("#FBF1A9");
+      } else {
+        ctx.setFillStyle("#fbb936");
+      }
+      ctx.fill();
+      ctx.save();
+      ctx.beginPath();
+      ctx.setFontSize(12); //设置文字字号大小
+      ctx.setFillStyle("#fff"); //设置文字颜色
+      ctx.setTextAlign("center"); //使文字垂直居中显示
+      ctx.setTextBaseline("middle"); //使文字水平居中显示
+      ctx.translate(w / 2, h / 2); //将原点移至圆形圆心位置
+      ctx.rotate((angle * (i + 2) - 20) * Math.PI / 180); //旋转文字，从 i+2 开始，因为扇形是从数学意义上的第四象限第一个开始的，文字目前的位置是在圆心正上方，所以起始位置要将其旋转2个扇形的角度让其与第一个扇形的位置一致。
+      ctx.fillText(item.name, 0, -(h / 2 * 0.8));
+      ctx.restore(); //保存绘图上下文，使上一个绘制的扇形保存住。
      
-      //绘制文字
-      // context.translate(900, 600);
-      // context.font = "100px Georgia";
-      // context.fillStyle = "#fff";
-      // context.fillText(item.name, -80, -60);
-      // scontextector.translate(-1000, -600);
-  
 
     }
-    context.draw()
+    that.loadImage();
+    ctx.draw();
   },
-  onLoad:function(opt) {
+  loadImage:function() { //绘制奖品图片，与绘制文字方法一致。
+
+    let that = this;
+    let data = this.data.awardList;
+    let Num = data.length;
+    let angel  = 360 / len;
+    let ctx = context;
+    for (let i = 0; i < Num; i++) {
+      ctx.save();
+      ctx.beginPath();
+      ctx.translate(w1, h1);
+      ctx.rotate(itemsArc * (i + 2) * Math.PI / 180);
+      ctx.drawImage("/icons/img/award.jpg", -(w1 * 0.2), -(h1 * 0.6), (w1 * 0.4), (h1 * 0.2));
+      ctx.restore();
+    }
+
+  },
+  onLoad: function(opt) {
     var _this = this;
     //圆点设置，顺时针设置
     var leftCircle = 7.5;
     var topCircle = 7.5;
     var circleList = [];
-    var zero_point = { left: 375, top: 375}
+    var zero_point = {
+      left: 375,
+      top: 375
+    }
     var radius = 325
     for (var i = 0; i < 24; i++) {
-      var theta = Math.PI / 12  //angel 15 度
+      var theta = Math.PI / 12 //angel 15 度
       var angel = theta * i
       var small_radius = 10
 
       topCircle = zero_point.top - radius * Math.cos(angel) - small_radius
-      leftCircle = zero_point.left - radius * Math.sin(angel) -  small_radius
-      
-      circleList.push({ topCircle: topCircle, leftCircle: leftCircle });
+      leftCircle = zero_point.left - radius * Math.sin(angel) - small_radius
+
+      circleList.push({
+        topCircle: topCircle,
+        leftCircle: leftCircle
+      });
     }
     this.setData({
       circleList: circleList
     })
     //圆点闪烁
-    setInterval(function () {
+    setInterval(function() {
       if (_this.data.colorCircleFirst == '#FFDF2F') {
         _this.setData({
           colorCircleFirst: '#FE4D32',
